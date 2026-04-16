cf2cc2d09631ae336e00b4b460defb5c49d83add
최민규 <153361663+alison0525@users.noreply.github.com>
2026-04-16 14:33:59 +0900

    feat: OAuth2 카카오 로그인 및 로그아웃 구현 (#21)
    * feat: OAuth2 로그인 구현 추가 구현 및 리펙토링
    
    * feat: 유저 로그아웃 기능 구현
    
    * refactor: 찜 기능 AuthUtil 적용 (하드코딩 memberId 제거)

--- backend/src/main/java/com/back/team11/domain/auth/service/AuthService.java
+++ backend/src/main/java/com/back/team11/domain/auth/service/AuthService.java
@@ -1,4 +1,34 @@
 package com.back.team11.domain.auth.service;
 
+import com.back.team11.domain.auth.repository.RefreshTokenRepository;
+import com.back.team11.domain.global.util.CookieUtil;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
 public class AuthService {
+
+    private final CookieUtil cookieUtil;
+    private final RefreshTokenRepository refreshTokenRepository;
+
+    @Transactional
+    public void logout(HttpServletRequest request, HttpServletResponse response) {
+        // 쿠키에서 refreshToken 꺼내기
+        String refreshToken = cookieUtil.getRefreshTokenFromCookie(request);
+
+        // DB에서 RefreshToken 삭제
+        if (refreshToken != null) {
+            refreshTokenRepository.findByToken(refreshToken)
+                    .ifPresent(refreshTokenRepository::delete);
+        }
+
+        // 쿠키 삭제
+        cookieUtil.deleteAccessTokenCookie(response);
+        cookieUtil.deleteRefreshTokenCookie(response);
+    }
 }
---
+++ backend/src/main/java/com/back/team11/domain/global/util/AuthUtil.java
@@ -0,0 +1,29 @@
+package com.back.team11.domain.global.util;
+
+import com.back.team11.domain.global.exception.CustomException;
+import com.back.team11.domain.global.exception.ErrorCode;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+
+@Component
+public class AuthUtil {
+    public Long getCurrentMemberId(){
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        if(authentication == null
+                || !authentication.isAuthenticated()
+                || authentication.getPrincipal().equals("anonymousUser")){
+            throw new CustomException(ErrorCode.UNAUTHORIZED);
+        }
+
+        return (Long) authentication.getPrincipal();
+    }
+
+    public boolean isAdmin() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        return authentication.getAuthorities()
+                .stream()
+                .anyMatch(authority -> authority.getAuthority().equals("ROLE_ADMIN"));
+    }
+}
