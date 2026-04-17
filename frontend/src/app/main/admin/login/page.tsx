ce89d7f115736d4fef359abe4dae33aeab6bbbbc
JunWoo Seo <csjws@naver.com>
2026-04-17 17:05:47 +0900

    feat: 관리자 로그인 API 적용

--- frontend/src/app/main/admin/login/page.tsx
+++ frontend/src/app/main/admin/login/page.tsx
@@ -1,14 +1,41 @@
-// app/admin/login/page.tsx
 "use client";
 
 import { useState } from "react";
+import { useRouter } from "next/navigation";
 
 export default function AdminLoginPage() {
+  const router = useRouter();
+
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
-  const handleLogin = () => {
-    console.log("로그인 시도", { email, password });
+  const handleLogin = async () => {
+    try {
+      const res = await fetch(
+        `${process.env.NEXT_PUBLIC_API_BASE_URL}/api/V1/admin/auth/login`,
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          credentials: "include", //쿠키 받기
+          body: JSON.stringify({
+            email,
+            password,
+          }),
+        }
+      );
+
+      if (!res.ok) {
+        throw new Error("로그인 실패");
+      }
+
+      //로그인 성공 → 관리자 페이지 이동
+      router.replace("/admin");
+    } catch (error) {
+      console.error(error);
+      alert("이메일 또는 비밀번호가 올바르지 않습니다.");
+    }
   };
 
   return (
