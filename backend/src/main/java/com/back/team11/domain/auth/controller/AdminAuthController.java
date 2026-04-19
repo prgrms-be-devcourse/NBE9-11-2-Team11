4e0136623e072dffeca25f81b6a38ef1492e96b0
BroJin <gudwls2777@naver.com>
2026-04-20 04:11:07 +0900

    refactor: Admin로그인 컨트롤러 -> AuthService로 리팩토링 & 리프레시 토큰 저장 삭제 추가

--- backend/src/main/java/com/back/team11/domain/auth/controller/AdminAuthController.java
+++ backend/src/main/java/com/back/team11/domain/auth/controller/AdminAuthController.java
@@ -1,19 +1,12 @@
 package com.back.team11.domain.auth.controller;
 
 import com.back.team11.domain.auth.dto.LoginRequestDto;
-import com.back.team11.domain.auth.dto.TokenResponseDto;
 import com.back.team11.domain.auth.service.AuthService;
 import com.back.team11.domain.auth.service.TokenReissueService;
 import com.back.team11.domain.global.rsData.RsData;
-import com.back.team11.domain.global.util.CookieUtil;
-import com.back.team11.domain.member.entity.Member;
-import com.back.team11.domain.member.service.MemberService;
-import com.back.team11.domain.security.JwtTokenProvider;
-import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -22,44 +15,23 @@ import org.springframework.web.bind.annotation.*;
 @RequiredArgsConstructor
 public class AdminAuthController {
 
-    private final JwtTokenProvider jwtTokenProvider;
-    private final MemberService memberService;
-    private final CookieUtil cookieUtil;
     private final TokenReissueService tokenReissueService;
     private final AuthService authService;
 
     @PostMapping("/login")
-    public ResponseEntity<RsData<TokenResponseDto>> login(
+    public ResponseEntity<RsData<Void>> login(
             @RequestBody LoginRequestDto loginRequestDto,
             HttpServletResponse response
     ) {
-        Member member = memberService.findByEmail(loginRequestDto.getEmail());
+        authService.adminLogin(loginRequestDto, response);
 
-        if (member != null &&
-                memberService.validatePassword(loginRequestDto.getPassword(), member.getPassword())) {
-
-            String accessToken = jwtTokenProvider.generateAccessToken(
-                    member.getId(),
-                    member.getRole().toString()
-            );
-
-            String refreshToken = jwtTokenProvider.generateRefreshToken(member.getId());
-
-            // 여기 변경
-            cookieUtil.addAccessTokenCookie(response, accessToken);
-            cookieUtil.addRefreshTokenCookie(response, refreshToken);
-
-            return ResponseEntity.ok(
-                    new RsData<>(
-                            "로그인이 성공적으로 되었습니다.",
-                            "200",
-                            null
-                    )
-            );
-        }
-
-        return ResponseEntity.status(HttpStatus.UNAUTHORIZED)
-                .body(new RsData<>("Unauthorized", "401-1", null));
+        return ResponseEntity.ok(
+                new RsData<>(
+                        "관리자 로그인이 성공적으로 되었습니다.",
+                        "200",
+                        null
+                )
+        );
     }
 
     @PostMapping("/refresh")
@@ -82,7 +54,7 @@ public class AdminAuthController {
         return ResponseEntity.ok(
                 new RsData<>(
                         "로그아웃이 성공적으로 되었습니다.",
-                        "200-1",
+                        "200",
                         null
                 )
         );
