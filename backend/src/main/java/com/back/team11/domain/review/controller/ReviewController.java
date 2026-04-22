dd0c2df3261cbabbca3d515721e7929e25ceac34
BroJin <71700516+Hyeong-Jin-Lee@users.noreply.github.com>
2026-04-22 16:13:34 +0900

    docs: auth, review Swagger 작성 (#54)
    * docs: auth, review Swagger 작성
    
    * docs: application.yaml에 media-type 정의

--- backend/src/main/java/com/back/team11/domain/review/controller/ReviewController.java
+++ backend/src/main/java/com/back/team11/domain/review/controller/ReviewController.java
@@ -7,6 +7,10 @@ import com.back.team11.domain.global.util.AuthUtil;
 import com.back.team11.domain.review.dto.ReviewRequestDto;
 import com.back.team11.domain.review.dto.ReviewResponseDto;
 import com.back.team11.domain.review.service.ReviewService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Pageable;
@@ -17,6 +21,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@Tag(name = "Review", description = "리뷰 관련 API")
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/V1/cafe/{cafeId}/reviews")
@@ -27,6 +32,11 @@ public class ReviewController {
 
     //리뷰 작성
     @PostMapping
+    @Operation(summary = "리뷰 생성")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "리뷰 생성 성공"),
+            @ApiResponse(responseCode = "409", description = "이미 리뷰를 작성한 카페"),
+    })
     public ResponseEntity<RsData<ReviewResponseDto>> createReview(
             @PathVariable Long cafeId,
             @Valid @RequestBody ReviewRequestDto requestDto
@@ -40,6 +50,11 @@ public class ReviewController {
 
     //리뷰조회
     @GetMapping
+    @Operation(summary = "리뷰 조회")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "리뷰 생성 성공"),
+            @ApiResponse(responseCode = "404", description = "존재하지 않는 카페"),
+    })
     public ResponseEntity<RsData<List<ReviewResponseDto>>> getReviews(
             @PathVariable Long cafeId
     ) {
@@ -49,6 +64,11 @@ public class ReviewController {
 
     //페이징 리뷰조회
     @GetMapping("/page")
+    @Operation(summary = "페이징 리뷰 조회")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "페이징 리뷰 조회 성공"),
+            @ApiResponse(responseCode = "404", description = "존재하지 않는 카페"),
+    })
     public ResponseEntity<RsData<PageResponse<ReviewResponseDto>>> getReviewsPage(
             @PathVariable Long cafeId,
             @PageableDefault(size = 10)
@@ -60,6 +80,12 @@ public class ReviewController {
 
     //리뷰수정
     @PutMapping("/{reviewId}")
+    @Operation(summary = "리뷰 수정")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "리뷰 수정 성공"),
+            @ApiResponse(responseCode = "403", description = "리뷰 작성자만 수정/삭제 가능"),
+            @ApiResponse(responseCode = "404", description = "존재하지 않는 리뷰"),
+    })
     public ResponseEntity<RsData<ReviewResponseDto>> updateReview(
             @PathVariable Long cafeId,
             @PathVariable Long reviewId,
@@ -72,6 +98,12 @@ public class ReviewController {
 
     //리뷰삭제
     @DeleteMapping("/{reviewId}")
+    @Operation(summary = "리뷰 삭제")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "리뷰 수정 성공"),
+            @ApiResponse(responseCode = "403", description = "리뷰 작성자만 수정/삭제 가능"),
+            @ApiResponse(responseCode = "404", description = "존재하지 않는 리뷰"),
+    })
     public ResponseEntity<RsData<Void>> deleteReview(
             @PathVariable Long cafeId,
             @PathVariable Long reviewId
